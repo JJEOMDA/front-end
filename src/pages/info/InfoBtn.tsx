556134163a8ce3be0d005230b0aeb409181de884
Kim-Haul <bokugawa@naver.com>
2023-02-01 16:32:02 +0900

    :lipstick: 정보 입력 페이지 UI 초안 잡기 (6)

--- src/pages/info/InfoBtn.tsx
+++ src/pages/info/InfoBtn.tsx
@@ -1,29 +1,7 @@
-import React, { useEffect, Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
-import Swal from 'sweetalert2';
 
 const InfoBtn = (props: { setTabIndex: Dispatch<SetStateAction<number>> }) => {
-  useEffect(() => {
-    // sweet-alert 모달창
-    const Toast = Swal.mixin({
-      toast: true,
-      position: 'center',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer);
-        toast.addEventListener('mouseleave', Swal.resumeTimer);
-      },
-    });
-    Toast.fire({
-      icon: 'warning',
-      title:
-        '<span style="font-size: 14px">이런! 아직 소개를 작성하지 않으셨군요!',
-      width: 340,
-    });
-  }, []);
-
   return (
     <Wrap>
       <div className="desc">당신에 대해 들려주세요.</div>
@@ -32,7 +10,7 @@ const InfoBtn = (props: { setTabIndex: Dispatch<SetStateAction<number>> }) => {
           props.setTabIndex((prev) => prev + 1);
         }}
       >
-        저는 이런 사람 입니다 ,
+        안녕하세요, 저는 이런 사람 입니다
       </button>
     </Wrap>
   );
@@ -46,7 +24,7 @@ const Wrap = styled.div`
     font-weight: 700;
     // text 서서히 나타나게 하기
     opacity: 0;
-    animation: fadeInText 3s 3s ease-out forwards;
+    animation: fadeInText 3s 0s ease-out forwards;
     @keyframes fadeInText {
       100% {
         opacity: 1;
@@ -63,6 +41,6 @@ const Wrap = styled.div`
     font-size: 1.7rem;
     height: 5rem;
     opacity: 0;
-    animation: fadeInText 4s 4s ease-out forwards;
+    animation: fadeInText 4s 2s ease-out forwards;
   }
 `;
