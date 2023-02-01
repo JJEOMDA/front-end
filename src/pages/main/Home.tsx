492b915024b575e2ed1ea3551e92d77481c02224
Kim-Haul <bokugawa@naver.com>
2023-02-01 19:40:12 +0900

    :heavy_plus_sign: react-slick 라이브러리 설치

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -3,12 +3,17 @@ import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <Wrap></Wrap>
-  )
+    <Wrap>
+      <Profile></Profile>
+    </Wrap>
+  );
 };
 
 export default Home;
 
-const Wrap = styled.div`
-
-`
+const Wrap = styled.div``;
+const Profile = styled.div`
+  width: 100%;
+  height: 15rem;
+  background-color: red;
+`;
