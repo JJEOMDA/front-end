5f5c8d56cf209d5944faf6c278bd35a27a565ba2
Kim-Haul <bokugawa@naver.com>
2023-02-02 09:41:43 +0900

    :lipstick: 메인 홈 화면 초안 퍼블리싱 (3)

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -7,6 +7,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import { RiErrorWarningFill } from 'react-icons/ri';
 import { TfiBackLeft } from 'react-icons/tfi';
 import { AiOutlineRight } from 'react-icons/ai';
+import Swal from 'sweetalert2';
 
 const Home = () => {
   // useNavigate 선언
@@ -22,6 +23,28 @@ const Home = () => {
     autoplay: true,
     autoplaySpeed: 4500,
     infinite: true,
+  };
+
+  // sweet-alert 모달창
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'center',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer);
+      toast.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+  });
+
+  const matchingWaiting = () => {
+    Toast.fire({
+      icon: 'info',
+      title:
+        '<span style="font-size: 14px">대기중입니다! 매칭이 시작되면 알려드릴게요!',
+      width: 340,
+    });
   };
 
   return (
@@ -57,12 +80,51 @@ const Home = () => {
             <AiOutlineRight />
           </li>
           <li>
+            <div>실시간 매칭 현황</div>
+            <AiOutlineRight />
+          </li>
+          <li>
             <div>문의하기</div>
             <AiOutlineRight />
           </li>
         </ul>
       </Content>
       <hr />
+      <MatchingCard>
+        <div className="card-box">
+          <div
+            className="card first"
+            onClick={() => {
+              matchingWaiting();
+            }}
+          >
+            <div className="card-title">매칭대기</div>
+            <div className="num">0</div>
+          </div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div
+            className="card second"
+            onClick={() => {
+              matchingWaiting();
+            }}
+          >
+            <div className="card-title">매칭중</div>
+            <div className="num">0</div>
+          </div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div
+            className="card third"
+            onClick={() => {
+              matchingWaiting();
+            }}
+          >
+            <div className="card-title">매칭완료</div>
+            <div className="num">0</div>
+          </div>
+        </div>
+      </MatchingCard>
     </Wrap>
   );
 };
@@ -72,7 +134,7 @@ export default Home;
 const Wrap = styled.div``;
 const Profile = styled.div`
   width: 100%;
-  height: 15rem;
+  height: 17rem;
   .title-name {
     font-size: 2.3rem;
     font-weight: 700;
@@ -87,13 +149,13 @@ const Profile = styled.div`
     }
   }
   .title-banner {
-    margin: 1rem 0 1rem 0;
+    margin: 2rem 0 2rem 0;
     display: flex;
     justify-content: center;
   }
 `;
 const StyledSlider = styled(Slider)`
-  width: 95%;
+  width: 100%;
   .slick-dots {
     position: absolute;
     bottom: 10px;
@@ -138,6 +200,62 @@ const Content = styled.div`
         background-color: rgba(0, 123, 255, 0.1);
         color: #35a3dc;
       }
+    }
+  }
+`;
+const MatchingCard = styled.div`
+  margin: 2rem 0 2rem 0;
+  .card-box {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    .card {
+      width: 60px;
+      height: 60px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-radius: 5px;
+      color: #fff;
+      cursor: pointer;
+      .num {
+        font-size: 2.3rem;
+      }
+      transition: all 0.2s ease-in-out;
+      &:hover {
+        filter: brightness(90%);
+      }
+    }
+    .card.first {
+      background: linear-gradient(
+          287.96deg,
+          rgba(0, 0, 0, 0.16) 15.1%,
+          rgba(0, 0, 0, 0) 83.32%
+        ),
+        #e2445c;
+    }
+    .card.second {
+      background: linear-gradient(
+          287.96deg,
+          rgba(0, 0, 0, 0.16) 15.1%,
+          rgba(0, 0, 0, 0) 83.32%
+        ),
+        #02c3bd;
+    }
+    .card.third {
+      background: linear-gradient(
+          287.96deg,
+          rgba(0, 0, 0, 0.16) 15.1%,
+          rgba(0, 0, 0, 0) 83.32%
+        ),
+        #fdab3d;
+    }
+    .circle {
+      width: 6px;
+      height: 6px;
+      background-color: #e1e1e1;
+      border-radius: 3px;
     }
   }
 `;
